0b5dbe5d90809c0ca27e9af011c42be4c10c9710
Mads Jon Nielsen <madsjon@gmail.com>
2025-05-08 14:58:47 +0200

    Fix sonarcloud maintainability finding (#12)
    Co-authored-by: Mads Jon Nielsen <mjn@cego.dk>
    Co-authored-by: nitlom <102284366+nitlom@users.noreply.github.com>

---
+++ headers/consts.go
@@ -0,0 +1,14 @@
+package headers
+
+const (
+	XForwardedProto  = "X-Forwarded-Proto"
+	XForwardedMethod = "X-Forwarded-Method"
+	XForwardedHost   = "X-Forwarded-Host"
+	XForwardedUri    = "X-Forwarded-Uri"
+	Accept           = "Accept"
+	UserAgent        = "User-Agent"
+	Cookie           = "Cookie"
+	Authorization    = "Authorization"
+	RemoteUser       = "Remote-User"
+	ContentType      = "Content-Type"
+)
